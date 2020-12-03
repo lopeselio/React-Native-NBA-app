860cfda1360c38582d37da9552c9590b7248f3df
lopeselio <lopeselio@gmail.com>
2020-12-03 19:04:57 +0530

    added form state for login/signup

--- app/components/auth/authForm.js
+++ app/components/auth/authForm.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
 import { View, Image, Text } from 'react-native'
+import Input from '../../utils/forms/input'
 
 class AuthForm extends Component {
+    state = {
+        type: 'Login',
+        action: 'Login',
+        actionMode: 'I want to register',
+        hasErrors: false,
+        form:{
+            email:{
+                value: "",
+                valid: false,
+                type: 'textinput',
+                rules:{
+                    isRequired: true,
+                    isEmail: true
+                }
+            },
+            password: {
+                value: "",
+                valid: false,
+                type: 'textinput',
+                rules:{
+                    isRequired: true,
+                    minLength: 6
+                }
+            },
+            confirmPassword: {
+                value: "",
+                valid: false,
+                type: 'textinput',
+                rules:{
+                    confirmPass:'password'
+                }
+            }
+        }
+    }
     render(){
         return(
             <View>
-                <Text>Hello Form</Text>
+                <Input 
+                    placeholder='Enter email'
+                    placeholderTextColor='#cecece'
+                    type={this.state.form.email.type}
+                    value={this.state.form.email.value}
+                    autoCapitalize={"none"}
+                    keyboardType={"email-address"}
+                />
             </View>
         )
     }
