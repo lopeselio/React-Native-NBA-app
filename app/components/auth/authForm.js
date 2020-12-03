a1f111a2ea9cd840b40427ff65f81a9bcacddb1e
lopeselio <lopeselio@gmail.com>
2020-12-03 18:13:41 +0530

    upadted Auth Screen

---
+++ app/components/auth/authForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Image, Text } from 'react-native'
+
+class AuthForm extends Component {
+    render(){
+        return(
+            <View>
+                <Text>Hello Form</Text>
+            </View>
+        )
+    }
+}
+
+export default AuthForm
+
--- app/components/auth/index.js
+++ app/components/auth/index.js
@@ -1,26 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
-  Button,
+  ScrollView,
+  ActivityIndicator,
   StyleSheet,
   View,
   Text
 } from 'react-native';
-class SignIn extends Component {
-  render() {
+import LogoComponent from './authLogo'
+import AuthForm from './authForm'
 
-    return (
-      <View>
-        <Text>Auth component</Text>
-      </View>
-    )
+class SignIn extends Component {
+  
+  state = {
+      loading: false
+  }  
+  render() {
+      if(this.state.loading){
+          return(
+            <View style={styles.loading}>
+                <ActivityIndicator />
+            </View>
+          )         
+      }else {
+        return (
+            <ScrollView style={styles.container}>
+              <View>
+                  <LogoComponent />
+                  <AuthForm />
+              </View>
+            </ScrollView>
+          )
+      }
+
+    
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff'
-//   }
-// })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1d428a',
+    padding: 50
+  },
+  loading: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent:'center'
+  }
+})
 
 export default SignIn
