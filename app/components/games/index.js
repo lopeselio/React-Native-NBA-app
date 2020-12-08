db6e9357042343381a833cf13935bb9336f2a2a4
lopeselio <lopeselio@gmail.com>
2020-12-08 16:02:12 +0530

    games

--- app/components/games/index.js
+++ app/components/games/index.js
@@ -1,25 +1,2 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
-class Games extends Component {
-  render() {
-
-    return (
-      <View>
-        <Text>Games component</Text>
-      </View>
-    )
-  }
-}
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff'
-//   }
-// })
-
+import { Games } from './Games';
 export default Games
---
+++ app/components/news/ArticleComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+export default class ArticleComponent extends Component {
+    render() {
+
+        return (
+            <View>
+                <Text>News Article component</Text>
+            </View>
+        );
+    }
+}
