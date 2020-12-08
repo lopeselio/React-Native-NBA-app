dbb911cc9943afa5ed28cfbf98ce8b97bdfd184f
lopeselio <lopeselio@gmail.com>
2020-12-08 16:02:04 +0530

    games

---
+++ app/components/games/Games.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+export default class Games extends Component {
+  render() {
+
+    return (
+      <View>
+        <Text>Games component</Text>
+      </View>
+    );
+  }
+}
